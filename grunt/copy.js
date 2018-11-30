dbe8f3bfe9946cb079eee07f7d0cd25a8a3f8180
William Belle <william.belle@gmail.com>
2018-11-30 20:02:14 +0100

    add copy task

---
+++ grunt/copy.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function (grunt, options) {
+  return {
+    buildChrome: {
+      files: [{
+        expand: true,
+        cwd: 'src/',
+        src: ['js/**', 'images/**', 'manifest.json'],
+        dest: 'dist/chrome/',
+        nonull: true
+      }]
+    },
+    buildFirefox: {
+      files: [{
+        expand: true,
+        cwd: 'src/',
+        src: ['js/**', 'images/**'],
+        dest: 'dist/firefox/',
+        nonull: true
+      }, {
+        expand: true,
+        cwd: 'src/',
+        src: ['manifest.firefox.json'],
+        dest: 'dist/firefox/',
+        nonull: true,
+        rename: function (destBase, destPath) {
+          return destBase + destPath.replace(
+            'manifest.firefox.json', 'manifest.json'
+          );
+        }
+      }]
+    }
+  };
+};
